a814f7fbe3c747933458340921e386078303833d
JacobRuiz3415 <jacobruiz428@gmail.com>
2026-09-15 11:13:37 -0700

    circle

--- painting.js
+++ painting.js
@@ -2,3 +2,6 @@ let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,80,80);
+
+ctx.fillStyle = "blue"
+ctx.arc(95,50,40,0,2 * Math.PI);
